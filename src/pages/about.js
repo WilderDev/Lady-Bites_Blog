8804278fc51c3ba2a1a64f8180597bc09aa3bc52
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-24 08:57:49 -0800

    REMOVE: SEO

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import Layout from "../components/layout";
-import SEO from "../components/seo";
+// import SEO from "../components/seo";
 
 import Styles from "../styles/AboutPage.module.scss";
 
 const About = () => (
   <Layout>
-    <SEO title="About" />
+    {/* <SEO title="About" /> */}
     <header className={Styles.container}>
       <h1>Hi from the About Page</h1>
       <p>I Lady. I Bite Things.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import Layout from "../components/layout";
-import SEO from "../components/seo";
+// import SEO from "../components/seo";
 
 import Styles from "../styles/HomePage.module.scss";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    {/* <SEO title="Home" /> */}
     <header className={Styles.container}>
       <h1>Lady-Bites Blog</h1>
       <h2>What That Mouth Do. . .</h2>
