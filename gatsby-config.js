6391ba431245f35a5844cdea41334153ee819f7d
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-24 10:06:00 -0800

    ADD: Nav Links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: `Lady Bites Blog`,
+    shortTitle: `Lady Bites`,
+    titleTemplate: "%s · Lady Bites Blog",
     description: `Blog website for Lady-Bites Brand - Recipes, Videos & Blogs`,
+    image: "./cookie-logo.png", // path to image in static folder
     author: `@LaurenLadyBites`, // tsk: ask for her twitter handle,
     siteUrl: `https://ladybites.netlify.app/`,
+    socials: {
+      instagram: `tsk`,
+      pintrest: `tsk`,
+    },
   },
   plugins: [
     `gatsby-plugin-preact`,
@@ -18,6 +25,86 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-readingtime-contentful",
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                keywords
+                author {
+                  name
+                  summary
+                }
+                managingEditor: author {name}
+                webMaster: author {name}
+                siteUrl
+                site_url: siteUrl
+                categories: keywords,
+                image: image
+                image_url: image
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulBlogPost } }) => {
+              return allContentfulBlogPost.edges.map(({ node: post }) => {
+                return {
+                  title: post.title,
+                  description: post.subTitle,
+                  date: post.publishDate,
+                  // author: `${post.author.firstName} ${post.author.lastName}`,
+                  // categories: site.siteMetadata.keywords.join(", "),
+                  url: `${site.siteMetadata.siteUrl}/blog/${post.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/blog/${post.slug}`,
+                  custom_elements: [{ "content:encoded": post.body.body }],
+                };
+              });
+            },
+            query: `
+              {
+                 allContentfulBlogPost(sort: {fields: publishDate, order: ASC}) {
+                   edges {
+                    node {
+                      slug
+                      title
+                      subTitle
+                      publishDate(formatString: "MMMM DD, YYYY")
+                      body {
+                        body
+                      }
+                      author {
+                        firstName
+                        lastName
+                      }
+                      
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Lady Bites Blog - RSS",
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -61,8 +148,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        createLinkInHead: true,
+      },
+    },
     `gatsby-plugin-offline`,
   ],
 };
