61d3d5293297782731bbf1895871075822581514
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-23 17:10:38 -0800

    More Cleanup

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>Hi from the About Page</h1>
+    <p>I Lady. I Bite things.</p>
+  </Layout>
+);
+
+export default About;
