4ee667eb51483a43c288edcfac0d89e80b3beb2d
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-24 11:12:23 -0800

    Fixed: Sharp module npm rebuild

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,7 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    "gatsby-plugin-readingtime-contentful",
-    `gatsby-plugin-react-helmet`,
+    // "gatsby-plugin-readingtime-contentful",
     // {
     //   resolve: `gatsby-plugin-feed`,
     //   options: {
@@ -139,13 +138,13 @@ module.exports = {
         scope: `.`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         createLinkInHead: true,
       },
     },
+    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-offline`,
   ],
 };
