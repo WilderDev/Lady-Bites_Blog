1344fff7abd4e2201390e9962332d4985b08e739
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-23 17:04:46 -0800

    INSTALL: Gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lady Bites Blog`,
+    description: `Little bites, big bites . . . Lady Bites`, // tsk: better slogan
+    author: `@`, // tsk: ask for her twitter handle
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +13,9 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -28,8 +29,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+
+    `gatsby-plugin-offline`,
   ],
-}
+};
