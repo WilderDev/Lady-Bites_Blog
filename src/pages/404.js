57a765f28d30d7d4e25cb36d34057a05577efffc
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-24 08:53:15 -0800

    MOVE: Layout/SEO o.O

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Layout from "../templates/layout";
-import SEO from "../templates/seo";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
