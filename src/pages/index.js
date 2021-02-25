ecdff492e7e46f4fb7e9bc38c46395e42493281d
William Wilder <WilliamJohnWilder@outlook.com>
2021-02-25 04:22:33 -0800

    ADD: Hero image & Landing Basic Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -8,8 +9,22 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <header className={Styles.container}>
-      <h1>Lady Bites Blog</h1>
-      <h2>What That Mouth Do!</h2>
+      <StaticImage
+        src="../images/cups-of-coffee-bg.jpg"
+        alt="Two cups of whip-cream decorated coffee on a wooden table"
+        placeholder="blurred"
+        layout="fullWidth"
+        loading="eager"
+        style={{ maxHeight: "100vh", minHeight: "100vh", zIndex: "-2" }}
+        transformOptions={{ shadow: "string" }}
+        quality={100}
+        objectFit
+        objectPosition
+      />
+      <section className={Styles.heroText}>
+        <h1>Lady Bites Blog</h1>
+        <h2>What That Mouth Do!</h2>
+      </section>
     </header>
   </Layout>
 );
